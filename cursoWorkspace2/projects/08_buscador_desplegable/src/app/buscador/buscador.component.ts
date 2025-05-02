35b5b43f2766f9fd76c4d43b6086f4d37068c6d9
Ariani <arianicarolina@gmail.com>
2025-05-02 17:52:07 +0200

    2/5

---
+++ cursoWorkspace2/projects/08_buscador_desplegable/src/app/buscador/buscador.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { Pagina } from './model/Pagina';
+
+
+@Component({
+  selector: 'app-buscador',
+  imports: [],
+  templateUrl: './buscador.component.html',
+  styleUrl: './buscador.component.css'
+})
+
+export class BuscadorComponent {
+
+tematica:string="";
+direccion:string="";
+datos:Pagina[]=[
+  new Pagina( "libros",   "http://www.puntolibro.es"),
+  new Pagina( "deportes",  "http://www.todofutbol.com"),
+  new Pagina("libros",  "http://www.fnac.es"),
+  new Pagina( "cocina", "http://www.fogones.es"),
+  new Pagina( "juegos", "http://www.game.es")
+];
+visible:boolean=false;//para usarlo después si queremos hacer visible o invisible
+const temaElegido:string="";
+const resultados:Pagina[]=[];
+
+
+//cuando damos al select debe mostrar las temáticas sin repetir
+//vuelvo a convertir en array para poder iterar en html
+cargarTematicas():string[]{
+ const ctematicas= new Set( this.datos.map(n=>n.tematica));
+return  Array.from(ctematicas);
+}
+
+//al seleccionar una temática debe dar como resultado las paginas con la temática
+buscar():void{
+ this.resultados=this.datos.filter((n, temaElegido)=>n.tematica==this.temaElegido);
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
